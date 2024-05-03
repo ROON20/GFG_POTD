a23896d20b11562e8662307522d4bd72d8cbc636
ROON20 <116735961+ROON20@users.noreply.github.com>
2024-05-03 14:11:39 +0530

    K distance from root

---
+++ may_2024/3rd_may.py
@@ -0,0 +1,23 @@
+# K distance from root
+
+class Solution:
+    def KDistance(self,root,k):
+        queue=[]
+        ans=[]
+
+        count=0
+        queue.append(root)
+        while queue and count<=k+1:
+            level=[]
+            for i in range(len(queue)):
+                node=queue.pop(0)
+                if node:
+                    level.append(node.data)
+                    queue.append(node.left)
+                    queue.append(node.right)
+
+            if level:
+                ans.append(level)
+                count+=1
+
+        return ans[k] if len(ans)>k else []
